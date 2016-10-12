6ed6b992ac0714b46548cf6c763c3abfaac75f3c
dorianpenaloza <dorianpenaloza@yahoo.com>
2016-10-11 20:27:45 -0500

    first pull request

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express'); //bringing in express library
 
+const app = express();
+
+app.get('/:message', function(request, response, next) {
+  const message = request.params.message;
+  return response.json(message);
+});
+
+const port = 3000;
+
+app.listen(port, function() {
+  console.log('Listening on port: ${port}');
+});
